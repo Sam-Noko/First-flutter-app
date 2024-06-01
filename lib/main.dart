8628f6249593cc2db03b82993f9793f5a62df8b7
NOKO <noko@NOKOs-MacBook-Air.local>
2024-06-01 15:39:21 +0400

    add container to main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -39,11 +39,30 @@ class _Home extends State<Home>{
         centerTitle: true,
         title: const Text('Counter', style: TextStyle(color: Colors.white,fontSize: 20.0, fontWeight: FontWeight.bold,),),
       ),
-      body:  Center(
-        child: Text(
-          '$_counter',
-          style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold,),
-          ),
+      body:  Container(
+        margin: const EdgeInsetsDirectional.symmetric(horizontal: 20.0, vertical: 20.0),
+        alignment: Alignment.center,
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 40.0,
+              backgroundColor: Colors.grey[200],
+              child: Icon(Icons.person_2, size: 50.0, color: Colors.grey[800],),
+            ),
+            SizedBox(height: 40.0,),
+            Text(
+              '$_counter',
+              style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold,),
+              ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
